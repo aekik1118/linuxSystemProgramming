6addbd5b5e8b580482dedb53e4a528e1039603ed
aekik1118 <aekik1118@naver.com>
2019-12-04 13:52:30 +0900

    write

---
+++ ch02/write.c
@@ -0,0 +1,30 @@
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <stdio.h>
+#include <errno.h>
+#include <string.h>
+
+extern int errno;
+
+int main(){
+
+    const char *buf = "My Ship is solid!";
+    const char *file_name = "wr_test";
+    ssize_t nr;
+
+    int fd;
+
+    fd = open(file_name, O_WRONLY | O_CREAT | O_TRUNC);
+
+    /*buf에 들어 있는 문자열을 'fd'에 기록한다*/
+    nr = write(fd, buf, strlen(buf));
+
+    if(nr == -1){
+        printf("error\n");
+    }
+    printf("success\n");
+
+    return 0;
+}
+
